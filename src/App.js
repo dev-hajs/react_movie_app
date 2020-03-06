6a538b4a002fac89965f01a59b2c5bacf11c72d0
하지수 <dev.hajs@gmail.com>
2020-03-06 23:20:42 +0900

    #6.3 Sharing Props Between Routes, using 'Link', changing Link props, sending movie details to another page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
 
 function App() {
   return (
     <HashRouter>
-      {/* Navigation (Link 사용한 경우) 은 Router 안에서만 유효함 */}
       <Navigation />
       <Route path="/" exact={true} component={Home}/>
       <Route path="/about" component={About}/>
+      <Route path="/movie-detail" component={Detail}/>
     </HashRouter>
   );
 }
