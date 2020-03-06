7b890685a55e74506389e2b3f34bf2cbe787aee3
하지수 <dev.hajs@gmail.com>
2020-03-06 22:17:35 +0900

    Updates

---
+++ precache-manifest.192f9ec921f0ce4eba41477ad9ac44eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bed0b99dedf930923521bb340f75e46",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f0a585a6042cfd08c74c",
+    "url": "/movie_app/static/css/main.de8df65d.chunk.css"
+  },
+  {
+    "revision": "1250df4c2d4d4eed078a",
+    "url": "/movie_app/static/js/2.c1e5e478.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.c1e5e478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a585a6042cfd08c74c",
+    "url": "/movie_app/static/js/main.2efdc8b1.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
