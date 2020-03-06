8de5bc997025599ccc66815c3c6229ccf30865a6
하지수 <dev.hajs@gmail.com>
2020-03-06 23:10:13 +0900

    #6.2 Building the Navigation, using HashRouter, using Navigator and.. little changing css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
     <HashRouter>
+      {/* Navigation (Link 사용한 경우) 은 Router 안에서만 유효함 */}
+      <Navigation />
       <Route path="/" exact={true} component={Home}/>
       <Route path="/about" component={About}/>
     </HashRouter>
